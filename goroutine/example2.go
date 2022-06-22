62f50f19211bc78ede0f703c53afa0f65b136724
Alazar Beyene <alazarbeyene99@gmail.com>
2022-06-22 10:42:02 +0300

    example2 how to solve racing condition in go using channel

---
+++ goroutine/example2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type NextId chan int
+
+func (ch NextId) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1> you have got %d </h1>", <-ch)
+}
+func counter(ch chan<- int) {
+	for i := 0; ; i++ {
+		ch <- i
+
+	}
+}
+func main() {
+	var nextId NextId = make(chan int)
+	go counter(nextId)
+	http.HandleFunc("/", nextId.handler)
+	fmt.Println("Listening on the port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
